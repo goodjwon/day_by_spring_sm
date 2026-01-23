2de00794d8b17f9ec9cf7a55f58e60d4be1eeeb3
천사무엘 <mfmfvlrm@naver.com>
2026-01-23 22:47:36 +0900

    2026.01.23 - Entity 추가 작성

--- src/main/java/com/example/spring/entity/Payment.java
+++ src/main/java/com/example/spring/entity/Payment.java
@@ -1,23 +1,28 @@
 package com.example.spring.entity;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 public class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String method;
+    @Column(name = "method")
+    private PaymentMethod method;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false, length = 40)
     private PaymentStatus status;
 
+    @Column(name = "amount", nullable = false)
     private int amount;
 
+    @Column(name = "card_company")
     private String cardCompany;
 
+    @Column(name = "card_number")
     private String cardNumber;
 
+    @Column(name = "installment_month")
     private int installmentMonth;
 }
